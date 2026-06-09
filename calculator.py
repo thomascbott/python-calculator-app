4234027ac5d6b9b98a9977608c4b1e3c6cad3f6f
Jombas72 <thomascbott@gmail.com>
2026-06-09 15:25:57 -0400

    minor reorganization and commenting

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
+# Helper Methods
 def get_two_numbers():
     x, y = input("Give two numbers separated by a space: ").split()
     return float(x), float(y)
 
+def print_result(z):
+    print(f"Result: {z}\n")
+
+# Four Basic Arithmetic Operation Methods
 def add(x, y):
     return x + y
 
@@ -15,7 +20,3 @@ def divide(x, y):
     if y == 0:
         return "Undefined: Division by zero"
     return x / y
-
-def print_result(z):
-    print(f"Result: {z}\n")
-
